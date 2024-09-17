5bb373d1b87fd2173af132ca2fac4fe91c30dff5
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-09-17 15:00:21 +0700

    init src

---
+++ IOS18-SwiftUI/App/IOS18_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS18_SwiftUIApp.swift
+//  IOS18-SwiftUI
+//
+//  Created by xqsadness on 17/9/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS18_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
