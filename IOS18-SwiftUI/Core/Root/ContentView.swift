d6fad639d6673e526a8356fc2f376bf7ea2c212c
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-09-17 16:41:49 +0700

    mesh gradient

--- IOS18-SwiftUI/Core/Root/ContentView.swift
+++ IOS18-SwiftUI/Core/Root/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isSheetPresented = false
+    @Namespace private var animation
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 15) {
+                    navigationScreen("Mesh Gradient") {
+                        MeshGradientView()
+                    }
+                }
+                .padding()
+            }
         }
-        .padding()
+    }
+    
+    @ViewBuilder
+    func navigationScreen<Content: View>(_ title: String, @ViewBuilder content: @escaping () -> Content) -> some View {
+        Button(action: {
+            isSheetPresented = true
+        }) {
+            HStack {
+                Text("\(title)")
+                    .font(.title3)
+                    .foregroundStyle(.text)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.up")
+                    .imageScale(.large)
+                    .foregroundStyle(.text)
+            }
+        }
+        .fullScreenCover(isPresented: $isSheetPresented) {
+            content()
+                .navigationTransition(.zoom(sourceID: UUID().uuidString, in: animation))
+        }
     }
 }
 
